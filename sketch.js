2867fec6a0c962b59c631dad4f44a6a0bc3f42de
sanwarakhandelwal <67106978+sanwarakhandelwal@users.noreply.github.com>
2020-07-16 20:19:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var car,wall;
+var speed,weigth;
+
+
+function setup() {
+  createCanvas(800,400);
+ car=createSprite(50,200,50,50);
+}
+
+function draw() {
+  background(255,255,255);
+  speed=random (55,90)
+  drawSprites();
+}
